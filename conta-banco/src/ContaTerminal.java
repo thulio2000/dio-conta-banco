8539e00a469a00ea3c280af96566cd0fd5dc871c
Túlio <lucastuliolima@gmail.com>
2023-09-22 14:39:56 -0300

    Adicionando lógica e variáveis da classe ContaBanco do projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -5,12 +5,18 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Insert your first name: ");
-        String name = scanner.next();
+        System.out.println("Insira o nome do titular da conta: ");
+        String nomeCliente = scanner.next();
 
-        System.out.println("Insert your age: ");
-        int age = scanner.nextInt();
+        System.out.println("Escolha os quatro dígitos para o número de sua conta: ");
+        int numero = scanner.nextInt();
 
-        System.out.println("Your name is " + name + " and you're " + age + " years old.");
+        System.out.println("Escolha seu número de agência, sendo três números, hífen e um quarto número: ");
+        String agencia = scanner.next();
+
+        System.out.println("Insira a quantia do seu primeiro depósito: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque.");
     }
 }
